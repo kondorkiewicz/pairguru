4dad14db2c3b3abb399d0b7553dcc21d8b6fb0d0
Konrad Ociepka <ociepkakonrad@gmail.com>
2017-12-27 16:31:07 +0100

    Fix falling movie specs

--- spec/factories/comment.rb
+++ spec/factories/comment.rb
@@ -1,9 +1,10 @@
 FactoryGirl.define do
   factory :comment do
+    association :user
+    association :movie
     title { Faker::Lorem.word }
     body { Faker::Lorem.sentence(3, true) }
     created_at { Time.now.utc }
-    user_id 1
     sequence(:movie_id) { |n| n }
   end
 end
--- spec/factories/movie.rb
+++ spec/factories/movie.rb
@@ -7,7 +7,7 @@ FactoryGirl.define do
 
     trait :with_comment do
       after(:create) do |movie|
-        create_list :comment, 1, user_id: 1, movie: movie
+        create_list :comment, 1, movie: movie
       end
     end
   end
